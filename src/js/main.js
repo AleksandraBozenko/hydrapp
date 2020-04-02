b33280eef06417d42948ea76baebfcdf126acae7
unknown <aleksandra.b.20018@gmail.com>
2020-04-02 17:10:24 +0200

    Add: Simple counter in JS

--- src/js/main.js
+++ src/js/main.js
@@ -14,9 +14,54 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-// Niżej możesz pisać.
+const counter = document.querySelector('.water__glass--counter');
+const add = document.querySelector('.water__add');
+const remove = document.querySelector('.water__remove');
 
+// counter.innerHTML = 0;
 
-console.log(`Hello world!`);
+// add.addEventListener('click', (e) =>{
+//   do{
+//     counter.innerHTML ++;
+//   }while(counter.innerHTML > 10);
+// })
 
+// remove.addEventListener('click', (e) =>{
+//   do{
+//     counter.innerHTML --;
+//   }while(counter.innerHTML >= 10);
+// })
+i = 0;
+add.addEventListener('click', (e) =>{
+  if(i > 10){
+    counter.innerHTML = {i}++;
+  };
+})
 
+remove.addEventListener('click', (e) =>{
+  if(i > 10){
+    counter.innerHTML = {i}--;
+  };
+})
+
+// remove.addEventListener('click', (e) =>{
+//   do{
+//     counter.innerHTML = {i}--;
+//   }while(i > 0);
+//   if(i <= 0){
+//     console.log(nie);
+//   }
+// })
+
+// counter.innerHTML = 0;
+// add.addEventListener('click', (e) => {
+//   if(counter.innerHTML < 10){
+//     counter.innerHTML = parseInt(counter.innerHTML) + 1;
+//     };
+// })
+
+// remove.addEventListener('click', (e) => {
+//   if(counter.innerHTML > 0){
+//     counter.innerHTML = parseInt(counter.innerHTML) - 1;
+//     };
+// })
