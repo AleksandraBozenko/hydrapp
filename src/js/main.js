631dc9bd24ba57b9212f642707784e22b717da4d
unknown <aleksandra.b.20018@gmail.com>
2020-04-07 10:27:30 +0200

    Repair: Counter value without localStorage

--- src/js/main.js
+++ src/js/main.js
@@ -20,63 +20,28 @@ const counter = document.querySelector('.water__glass--counter');
 const add = document.querySelector('.water__add');
 const remove = document.querySelector('.water__remove');
 
-// Licznik szklanek
 
-// i = 0;
-// add.addEventListener('click', (e) =>{
-//   if(i > 10){
-//     counter.innerHTML = {i}++;
-//   };
-// })
 
-// remove.addEventListener('click', (e) =>{
-//   if(i > 10){
-//     counter.innerHTML = {i}--;
-//   };
-// })
-const sumUp = document.querySelector('.water__sum');
+//Licznik szklanek
 
-sumUp.innerHTML = `Czas na łyk wody!`;
-
-counter.innerHTML = 0;
 
 add.addEventListener('click', (e) => {
-  if(counter.innerHTML < 10){
-    counter.innerHTML = parseInt(counter.innerHTML) + 1;
-    };
-  localStorage.setItem('counter', counter.innerHTML);
-  sumUp.innerHTML = `Ilość szklanek wody na dziś: ${counter.innerHTML}`;
+  counter.innerHTML = parseInt(counter.innerHTML) + 1;
+  // localStorage.setItem('counter', currentValue);
+
 })
 
 remove.addEventListener('click', (e) => {
-  if(counter.innerHTML > 0){
-    counter.innerHTML = parseInt(counter.innerHTML) - 1;
+
+const currentValue = parseInt(counter.innerHTML);
+
+  if(currentValue > 0){
+    counter.innerHTML = currentValue - 1;
+  //  localStorage.setItem('counter', currentValue);
     };
-   localStorage.setItem('counter', counter.innerHTML);
-   sumUp.innerHTML = `Ilość szklanek wody na dziś: ${counter.innerHTML}`;
 })
 
-// sumUp.innerHTML = `Czas na łyk wody!`;
 
-if (localStorage.getItem('counter')){
-  counter.innerHTML = localStorage.getItem('counter');
-}
-
-// Funkcja nieużyta
-
-// sumUp.innerHTML = `wypiłeś ${counter.innerHTML} szklanek wody`;
-
-// function description() {
-// if (counter.innerHTML = 1){
-//   sumUp.innerHTML = `wypiłeś ${counter.innerHTML} szklankę wody`;
-// }
-// else if(counter.innerHTML = 0 ){
-//   sumUp.innerHTML = `wypiłeś ${counter.innerHTML} szklanek wody`;
-// }
-// else if(counter.innerHTML > 1){
-//   sumUp.innerHTML = `wypiłeś ${counter.innerHTML} szklanki wody`;
-// }
-// else{
-//   sumUp.innerHTML = `wypiłeś ${counter.innerHTML} szklanek wody`;
-// }
+// if (localStorage.getItem('counter')){
+//   counter.innerHTML = localStorage.getItem('counter', currentValue);
 // }
